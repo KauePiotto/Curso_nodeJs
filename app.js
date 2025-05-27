db90afc9315c566dbe4c41c16a3b2e1013caa8af
KauePiotto <kauepiotto@gmail.com>
2025-05-27 17:47:37 -0300

    Aula 10 - Endepoints

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-console.log('Oie, em breve serei uma API ')
+import express, { request, response } from "express";
 
+const servidor = express();
+
+servidor.get('/helloworld', (req, resp) =>{
+    //Código do endpoint
+
+    resp.send('Hello world!!!!');
+})
+
+servidor.get('/mensagem/boasvindas', (req, resp) =>{
+    resp.send('Olá, sejam bem-vindos e bem-vindas! ');
+})
+
+servidor.get('/v2/mensagem/boasvindas', (req, resp) =>{
+    resp.send('Que bom que você está aqui! s2');
+})
+
+servidor.get('/mensagem/ocupado', (req, resp) =>{
+    resp.send('Estou ocupado no momento.');
+})
+
+servidor.get('/mensagem/ocupado/recado', (req, resp) =>{
+    resp.send('Estou ocupado, deixa uma mensagem no email xxxxxxx.');
+})
+
+servidor.listen(
+    5001, 
+    () => console.log('----> API subiu com sucesso na porta 5001!'));
