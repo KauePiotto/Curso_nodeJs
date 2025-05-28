fa063a15aee73b5b0af2cdbfe3c5d44bfd213e6d
KauePiotto <kauepiotto@gmail.com>
2025-05-28 16:01:54 -0300

    Aula 25 - Roteamento

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
 import 'dotenv/config.js';
 import express from 'express';
 import cors from 'cors';
-
-import calculadoraController from './controller/calculadoraController.js';
-import exerciciosController from './controller/exerciciosController.js';
-import lojaController from './controller/lojaController.js';
-import mensagemController from './controller/mensagemController.js';
-import usuarioController from './controller/usuarioController.js';
+import adicionarRotas from './rotas';
 
 const servidor = express();
 
@@ -16,16 +11,8 @@ servidor.use(cors());
 //Permite usar parâmetros de corpo na minha aplição
 servidor.use(express.json());
 
-//Usado para liberar a pasta storage/perfil para carregar a foto na Web
-servidor.use('/storage/perfil', express.static('./storage/perfil'))
-servidor.use('/storage/album', express.static('./storage/album'))
-
-//Criando vinculo entre o servidor principal e o arquivo controller
-servidor.use(calculadoraController);
-servidor.use(exerciciosController);
-servidor.use(lojaController);
-servidor.use(mensagemController);
-servidor.use(usuarioController);
+//Adicionando as Rotas
+adicionarRotas(servidor);
 
 //Porta da API
 const PORTA = process.env.PORTA;
